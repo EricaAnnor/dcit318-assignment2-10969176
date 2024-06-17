43df4b5712f0b84bfa147b598a05d6c4e6138290
lit <103055119+EricaAnnor@users.noreply.github.com>
2024-06-17 13:12:06 +0000

    interface

---
+++ oopInterface/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace oopInterface
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            car.Move();
+
+            IMovable bicycle = new Bicycle();
+            bicycle.Move();
+        }
+    }
+}
